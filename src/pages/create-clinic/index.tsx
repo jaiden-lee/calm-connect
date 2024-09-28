d001cb06d5e2141e2f5b124b706b3b5693d8dbad
Theo <theomhalpern@gmail.com>
2024-09-28 02:08:39 -0400

    added create clinic funtionality

--- src/pages/create-clinic/index.tsx
+++ src/pages/create-clinic/index.tsx
@@ -1,9 +1,10 @@
 import { UserContext } from "@/utils/context";
 import { createClient } from "@/utils/supabase/component";
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { redirect } from "next/navigation";
 import { type FormEvent, useContext, useState } from "react";
 import { useNotifications } from '@toolpad/core/useNotifications';
+import router from "next/router";
 
 function CreateClinic() {
     const supabase = createClient();
@@ -16,26 +17,50 @@ function CreateClinic() {
     });
 
     if (user == null) {
-        redirect(`/`)
+        return ("need to figure out how to redirect")
     }
 
     const createClinicInDB = async (e: FormEvent) => {
+        e.preventDefault();
         e.stopPropagation();
-        const { error } = await supabase
+        const { data, error } = await supabase
             .from('clinics')
-            .insert({ name: formData.clinicName, welcome_message: formData.clinicWelcomeMessage, user_id: user.id })
-            
-        notifications.show('Error: ' + error, {
-            severity: "error",
-            autoHideDuration: 3000,
+            .insert({ name: formData.clinicName, welcome_message: formData.clinicWelcomeMessage, id: user.id })
+            .select()
+        if (error != null) {
+            notifications.show('Error: ' + error.message, {
+                severity: "error",
+                autoHideDuration: 3000,
             });
+        } else {
+            notifications.show("Successfully created" + data[0].name, {
+                autoHideDuration: 3000,
+            })
+            router.push("/therapists")
+        }
     }
 
     return (
         <form onSubmit={createClinicInDB}>
-            Create your Clinic
-            <TextField id="clinicName" label="Clinic Name" name="clinicName" required />
-            <TextField id="clinicWelcomeMessage" label="Clinic Welcome Message" name="clinicWelcomeMessage" />
+            Create your Clinic <br />
+            <TextField
+                onChange={(e) => setFormData((prev) => { return { ...prev, "clinicName": e.target.value } })}
+                value={formData.clinicName}
+                id="clinicName"
+                label="Clinic Name"
+                name="clinicName"
+                margin="normal"
+                required /> <br />
+            <TextField
+                onChange={(e) => setFormData((prev) => { return { ...prev, "clinicWelcomeMessage": e.target.value } })}
+                value={formData.clinicWelcomeMessage}
+                id="clinicWelcomeMessage" 
+                label="Clinic Welcome Message" 
+                name="clinicWelcomeMessage"
+                multiline
+                margin="normal"
+                minRows={2} /> <br />
+            <Button type="submit">Submit</Button>
         </form>
     );
 }
